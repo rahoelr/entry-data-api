4334ad0d2550af991087dd42bbe022ca3a6a9104
rahul rahmatullah <rahul.rahmatullah@mail.ugm.ac.id>
2024-11-13 22:06:55 +0700

    refactor : change ID column to UUID in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
 
     protected $fillable = [
         'name',
