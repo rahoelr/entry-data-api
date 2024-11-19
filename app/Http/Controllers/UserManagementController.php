bf6ec66510901bb5685e3349469dfc58bcfb5fac
rahul rahmatullah <rahul.rahmatullah@mail.ugm.ac.id>
2024-11-19 16:42:06 +0700

    feat : add controller management user

---
+++ app/Http/Controllers/UserManagementController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+
+
+class UserManagementController extends Controller
+{
+    //get all users
+    public function index(){
+        $users = User::all();
+        return response()->json($users, 200);
+    }
+
+
+    //create new users
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'username' => 'required|string|unique:users',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8',
+            'role' => 'required|in:data_entry,user_kementerian,manager',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+        ]);
+
+        return response()->json(['message' => 'Akun berhasil ditambahkan', 'user' => $user], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Cari user berdasarkan UUID
+        $user = User::where('id', $id)->first();
+
+        if (!$user) {
+            return response()->json(['message' => 'User tidak ditemukan'], 404);
+        }
+
+        $request->validate([
+            'name' => 'nullable|string|max:255',
+            'username' => [
+                'nullable',
+                'string',
+                Rule::unique('users')->ignore($user->id),
+            ],
+            'email' => [
+                'nullable',
+                'string',
+                'email',
+                Rule::unique('users')->ignore($user->id),
+            ],
+            'password' => 'nullable|string|min:8',
+            'role' => 'nullable|in:data_entry,user_kementerian,manager',
+        ]);
+
+        $user->update([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => $request->filled('password') ? Hash::make($request->password) : $user->password,
+            'role' => $request->role,
+        ]);
+
+        return response()->json(['message' => 'Akun berhasil diubah', 'user' => $user], 200);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        if (!$user) {
+            return response()->json(['message' => 'User tidak ditemukan'], 404);
+        }
+        $user->delete();
+        return response()->json(['message' => 'Akun berhasil dihapus'], 200);
+    }
+}
