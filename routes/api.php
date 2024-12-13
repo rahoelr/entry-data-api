cfd22bff8a10cda42acd696d2b224913e0767b06
rahul rahmatullah <rahul.rahmatullah@mail.ugm.ac.id>
2024-12-13 21:57:31 +0700

    refactor : deleted unused code in routes api

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\EntryInstitutionController;
 use App\Http\Controllers\EntryuserController;
 use App\Http\Controllers\UserManagementController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SyncController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/customization/current', [CustomizationController::class, 'index']);
@@ -15,17 +14,14 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::middleware('role:manager')->group(function () {
-        Route::get('/manager/dashboard', function () {
-            return response()->json(['message' => 'Welcome Manager']);
-        });
         //kustomisasi
         Route::apiResource('/customization',CustomizationController::class);
         Route::get('customization/active-color', [CustomizationController::class, 'show']);
 
         //sinkronisasi
-        Route::post('/sync/save', [SyncController::class, 'saveEndpoint']);
-        Route::post('/sync/now', [SyncController::class, 'syncNow']);
-        Route::get('/sync/logs', [SyncController::class, 'getLogs']);
+//        Route::post('/sync/save', [SyncController::class, 'saveEndpoint']);
+//        Route::post('/sync/now', [SyncController::class, 'syncNow']);
+//        Route::get('/sync/logs', [SyncController::class, 'getLogs']);
 
         //user management
         Route::apiResource('/users',UserManagementController::class);
@@ -33,9 +29,6 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::middleware('role:data_entry')->group(function () {
-        Route::get('/data-entry/dashboard', function () {
-            return response()->json(['message' => 'Welcome Data Entry']);
-        });
         Route::apiResource('/entry-user',EntryuserController::class);
         Route::get('/entry-user/user/{userId}', [EntryuserController::class, 'showByUserId']);
         Route::put('/entry-user/status/{id}', [EntryuserController::class, 'updateStatus']);
